6e00f301eafd4bd98dfc99d480fdbedfcf25cf29
Envl <yyaomingm@gmail.com>
2019-11-20 11:51:51 +0100

    preview

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import {
   SidebarInjector,
   SidebarItem,
   CubeMenu,
+  FilterGroup,
 } from 'oapack'
 import './_index.scss'
 
@@ -21,20 +22,127 @@ function Me(props) {
         <div>a Developer</div>
       </DropDown>
       <img src='/me.jpg' alt='' />
+      <Card className='card-i'>
+        <h1>I</h1>
+      </Card>
+      <FilterGroup
+        className='block-filters'
+        filters={[
+          {name: 'c', pushed: false},
+          {name: 'o', pushed: false},
+          {name: 'd', pushed: true},
+          {name: 'e', pushed: true},
+
+          {name: 's', pushed: false},
+          {name: 'i', pushed: false},
+          {name: 'g', pushed: false},
+          {name: 'n', pushed: false},
+          {name: ':)', pushed: true, disabled: true},
+        ]}
+        // defaultIndex/={0}
+        onFilterUpdate={e => console.log(e)}
+      />
+    </div>
+  )
+}
+
+function Contents(props) {
+  return (
+    <div className='content'>
       <CubeMenu
-        className='cube'
+        className='home-cube'
         u=':)'
         f={
           <Link href='/CV_Ming_YAO.pdf'>
-            <a target='__blank'>Resume</a>
+            <a className='oa-underline' target='__blank'>
+              Resume
+            </a>
           </Link>
         }
         l={
-          <a href='https://github.com/envl' target='__blank'>
+          <a
+            className='oa-underline'
+            href='https://github.com/envl'
+            target='__blank'>
             Github
           </a>
         }
       />
+      <Card />
+      <Button type='toggle'>test</Button>
+      <button>normal</button>
+      <DropDown title='I am a'>
+        <div>UX Designer</div>
+        <div>Full Stack Developer</div>
+      </DropDown>
+      <Card>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+      </Card>
+      {/* <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}></div> */}
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <Card>
+        <h1>t</h1>
+      </Card>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
+      <h1>t</h1>
     </div>
   )
 }
@@ -59,31 +167,7 @@ function HomePage() {
       }>
       <div className='home flex-h-center'>
         <Me />
-        <div className='content'>
-          <Card />
-          <Button type='toggle'>test</Button>
-          <button>normal</button>
-          <DropDown title='I am a'>
-            <div>UX Designer</div>
-            <div>Full Stack Developer</div>
-          </DropDown>
-          <p>Welcome to your new Gatsby site.</p>
-          <p>Now go build something great.</p>
-          <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}></div>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-          <h1>t</h1>
-        </div>
+        <Contents />
       </div>
     </SidebarInjector>
   )
