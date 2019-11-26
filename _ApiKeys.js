f9621e767ad69875c0f91066632d76430beee708
GnimOay <yyaomingm@gmail.com>
2019-11-26 02:01:34 +0100

    no secret

---
+++ _ApiKeys.js
@@ -0,0 +1,10 @@
+export const firebase_Config = {
+  apiKey: 'AIzaSyCJdlzhnQdsONLmv1KGQIHY9FhiChyqCBA',
+  authDomain: 'oa-site.firebaseapp.com',
+  databaseURL: 'https://oa-site.firebaseio.com',
+  projectId: 'oa-site',
+  storageBucket: 'oa-site.appspot.com',
+  messagingSenderId: '926252173661',
+  appId: '1:926252173661:web:8b170a46a40e78405380b4',
+  measurementId: 'G-YW09J10NBL',
+}
