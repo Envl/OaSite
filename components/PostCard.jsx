2b743fb59e036b0a4322490da0f77e4e75235519
GnimOay <yyaomingm@gmail.com>
2019-11-28 01:16:37 +0100

    add tags and update link

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
 import {Card} from 'oapack'
 import Link from 'next/link'
+import './_postCard.scss'
 
-const PostCard = props => {
+const PostCard = ({data, ...props}) => {
   return (
     <Card
       // {...props}
       className={`post ${props.className ? props.className : ''}`}>
-      <div>
-        {props.title &&
-          (typeof props.url !== 'undefined' ? (
-            <a href={props.url} className='post-title oa-underline'>
-              {props.title}
+      <div className='card-text'>
+        {data.title &&
+          (typeof data.url !== 'undefined' ? (
+            <a href={data.url} className='post-title oa-underline'>
+              {data.title}
             </a>
           ) : (
             <Link href={props.href}>
-              <span className='post-title oa-underline'>{props.title}</span>
+              <span className='post-title oa-underline'>{data.title}</span>
             </Link>
           ))}
         <span className='post-content'>{props.children}</span>
+        {data.tags && (
+          <div className='tag-list'>
+            {data.tags.map(tag => (
+              <div className={`post-tag ${tag === 'WIP' ? 'tag-wip' : ''}`}>
+                {tag}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      {props.coverUrl && (
-        <img className='post-img' src={props.coverUrl} alt='' />
-      )}
+      {data.coverUrl && <img className='post-img' src={data.coverUrl} alt='' />}
     </Card>
   )
 }
