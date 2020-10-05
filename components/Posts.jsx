93d4f36dad2c1ff7b85cecbfe96ebcbfe221675e
GnimOay <yyaomingm@gmail.com>
2020-10-05 14:34:49 +0200

    fix hover error on <Link/>

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -7,13 +7,16 @@ import './_posts.scss'
 export default function Posts({zmd}) {
   return (
     <Card className='async-article-posts'>
-      {Object.values(zmd.slice(-7, zmd.length)).map((v, i) => (
-        <Link href={'/' + v.id.split('-').join('')} key={'post' + i}>
-          <a className='async-article-posts__link' target='_blank'>
-            {v.Name}
-          </a>
-        </Link>
-      ))}
+      {Object.values(zmd.slice(-7, zmd.length)).map((v, i) => {
+        const url = '/' + v.id.split('-').join('')
+        return (
+          <Link href={url} key={'post' + i} as={url}>
+            <a className='async-article-posts__link' target='_blank'>
+              {v.Name}
+            </a>
+          </Link>
+        )
+      })}
       <h3>芝麻地 - 关于设计与编程的Newsletter</h3>
     </Card>
   )
