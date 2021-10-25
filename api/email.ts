8ac47330eb39c78a4d2af158739fb26b20d7f471
envl <yyaomingm@gmail.com>
2021-10-25 17:28:40 +0800

    add email api

---
+++ api/email.ts
@@ -0,0 +1,54 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import nodemailer from 'nodemailer'
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: 'smtp.exmail.qq.com',
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: 'hi@gnimoay.com',
+    pass: process.env.EXMAIL_SECRET,
+  },
+})
+
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  const {
+    headers: { authorization },
+  } = req
+
+  if (authorization !== process.env.EMAIL_TOKEN) {
+    return res.status(300).send('invalid token')
+  }
+
+  try {
+    const mail: Email = req.body
+    // @ts-ignore
+    const msg = await transporter.sendMail(mail)
+    return res.status(200).send(msg)
+  } catch (error) {
+    return res.status(400).json({
+      error: 'invalid body format, should be JSON formatted email',
+      msg: error,
+    })
+  }
+}
+
+type Email = {
+  from: string
+  to: string
+  subject: string
+  text: string
+  html: string
+}
+
+/*
+Email Object Example
+{
+  from: '"Fred Foo 👻" <hi@gnimoay.com>', // sender address
+  to: 'xxx@gmail.com', // list of receivers
+  subject: 'Hello ✔', // Subject line
+  text: 'Hello world?', // plain text body
+  html: '<b>Hello world?</b>', // html body
+}
+**/
